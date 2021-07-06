b37b87208849bcdeda00ef48f3c91fb18c01231a
nimezot1 <nimezot@gmail.com>
2021-07-06 17:36:32 -0300

    testes do radio botton

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,12 +1,30 @@
 describe("tickets", () => {
     beforeEach(() => cy.visit("https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html"));
     
-    it("fills all the text input fields", () => {
+    it("Preenchendo os input fields", () => {
         cy.get("#first-name").type("Tiago");
         cy.get("#last-name").type("Ferreira");
         cy.get("#email").type("tiago@teste.com.br");
+
+        //Escolhendo select \/
         cy.get("#ticket-quantity").select("2");
+
+        //Escolhendo o radio button \/
+        cy.get("#vip").check();
+
+        //Escolhendo o radio button \/
+        cy.get("#friend").check();
+        cy.get("#publication").check();
+        cy.get("#social-media").check();
+        cy.get("#friend").uncheck();
+        cy.get("#publication").uncheck();
+        cy.get("#social-media").uncheck();
+        cy.get("#friend").check();
+        cy.get("#publication").check();
+        cy.get("#social-media").check();
+
         cy.get("#requests").type("é nóix mano");
+        cy.get("#agree").check(); 
         cy.get("#signature").type("O profeta");
     });
     

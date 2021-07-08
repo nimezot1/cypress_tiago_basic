49483bc027fecc9801eeea8dd770636ea11356e3
nimezot1 <nimezot@gmail.com>
2021-07-08 10:53:34 -0300

    Finalização dos comandos (commands.js)

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -2,241 +2,53 @@ describe("tickets", () => {
     beforeEach(() => cy.visit("https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html"));
     
     const usuario = {
-        primeiroNome: "Jose",
-        ultimoNome:   "João",
-        email:        "josejoao@teste.com.br",
+        primeiroNome: "Tiago",
+        ultimoNome:   "Ferreira",
+        email:        "tia1go@teste.com.br",
+        frase:        "é nóix mano",
+        assinatura:   "O Profeta",
+        
     };
 
-    const titulo = "TICKETBOX";
-    const primeiro_Nome = "Tiago";
-    const ultimo_Nome = "Ferreira";
-    const nomeInteiro = `${primeiro_Nome} ${ultimo_Nome}`;
-    const email = "tiago@teste.com.br";
-    const emailInvalido = "tiago----teste.com.br";
-    const frase = "é nóix mano";
-    const assinatura = "O Profeta";
+  const dadosValidaTexto = {
+      primeiroNome: "Tiago", 
+      ultimoNome: "Ferreira",
+      radio: "#general", 
+      qtn: "1",
+  };
+
+  const outrosDados = {
+    titulo:       "TICKETBOX",
+    emailInvalido:"tiago----teste.com.br",
+    email:        "asddadasdas@teste.com.br",
+  }
 
     it("Validando preenchimento os input fields", () => {
-        cy.get("#first-name")
-          .type(primeiro_Nome);
-        
-        cy.get("#last-name")
-          .type(ultimo_Nome);
-
-        cy.get("#email")
-          .type(email);
-
-        //Escolhendo o checkbox \/
-        cy.get("#friend")
-          .check();
-
-        cy.get("#publication")
-          .check();
-
-        cy.get("#social-media")
-          .check();
-
-        cy.get("#friend")
-          .uncheck();
-
-        cy.get("#publication")
-          .uncheck();
-
-        cy.get("#social-media")
-          .uncheck();
-
-        cy.get("#friend")
-          .check();
-
-        cy.get("#publication")
-          .check();
-
-        cy.get("#social-media")
-          .check();
-
-        cy.get("#requests")
-          .type(frase);
-
-        cy.get("#agree")
-          .check(); 
-
-        cy.get("#signature")
-          .type(assinatura);
+        cy.preencheCamposTexto(usuario);
+        cy.preencheCheckbox();
+        cy.unCheckbox();
+        cy.preencheCheckbox();
+        cy.aceitaCheck();
     });
 
+    // ESTÁ FAZENDO APENAS UMA VALIDAÇÃO
     it("Validando select, radio e texto", () => {
-        cy.get("#first-name")
-          .type(primeiro_Nome);
-
-        cy.get("#last-name")
-          .type(ultimo_Nome);
-        
-        //Escolhendo select  1 com radio button GENERAL \/
-        cy.get("#general")
-         .check();
-
-        cy.get("#ticket-quantity")
-          .select("1");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 1 General Admission ticket. I understand that all ticket sales are final.`);
-  
-        cy.get("#ticket-quantity")
-          .select("2");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 2 General Admission tickets. I understand that all ticket sales are final.`);
-          
-        cy.get("#ticket-quantity")
-          .select("3");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 3 General Admission tickets. I understand that all ticket sales are final.`);
-  
-        cy.get("#ticket-quantity")
-          .select("4");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 4 General Admission tickets. I understand that all ticket sales are final.`);
-  
-
-        //Escolhendo select  1 com radio button VIP \/
-        cy.get("#vip")
-         .check();
-
-        cy.get("#ticket-quantity")
-          .select("1");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 1 VIP ticket. I understand that all ticket sales are final.`);
-  
-        cy.get("#ticket-quantity")
-          .select("2");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 2 VIP tickets. I understand that all ticket sales are final.`);
-          
-        cy.get("#ticket-quantity")
-          .select("3");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 3 VIP tickets. I understand that all ticket sales are final.`);
-  
-        cy.get("#ticket-quantity")
-          .select("4");
-        cy.get(".agreement p")
-          .should("contain", `I, ${nomeInteiro}, wish to buy 4 VIP tickets. I understand that all ticket sales are final.`);
-        
+        cy.validaInformacoesDeTexto(dadosValidaTexto);  
     });
 
     it("Validando titulo", () => {
-        cy.get("header h1")
-          .should("contain", titulo);
+       cy.validarTitulo(outrosDados);
     });
 
-    it("Validando email invalido", () => {
-        cy.get("#email")
-          .as("email")
-          .type(emailInvalido);
-
-        cy.get("#email.invalid")
-          .should("exist");
-        
-        cy.get("@email")
-        .clear()
-          .type(email);
-
-        cy.get("#email.invalid")
-          .should("not.exist");
+    it("Validando email", () => {
+       cy.validarEmail(outrosDados);
     });
 
-    it ("Validando campos obrigatórios e botão reset", () => {
-        //validação dos campos obrigatórios
-        cy.get("button[type='submit']")
-          .as("botaoEnviar")
-          .should("be.disabled");
-
-        cy.get("#first-name")
-          .type(primeiro_Nome);
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-
-        cy.get("#last-name")
-          .type(ultimo_Nome);
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-          
-        cy.get("#email")
-          .type(email);
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-
-        cy.get("#friend")
-          .check();
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-
-        cy.get("#publication")
-          .check();
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-
-        cy.get("#social-media")
-          .check();
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-          
-        cy.get("#requests")
-          .type(frase);
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-
-        cy.get("#signature")
-          .type(assinatura);
-        cy.get("@botaoEnviar")
-          .should("be.disabled");
-
-        cy.get("#agree")
-          .check();
-        cy.get("@botaoEnviar")
-          .should("not.be.disabled");
-        
-        //validação do botão reset
-        cy.get("button[type='reset']")
-          .click();
-
-        cy.get("#first-name")
-          .should('be.empty');
-
-        cy.get("#last-name")
-          .should('be.empty');
-
-        cy.get("#email")
-          .should('be.empty');
-
-        cy.get("#friend")
-          .should('be.empty');
-          
-        cy.get("#publication")
-          .should('be.empty');
-
-        cy.get("#social-media")
-          .should('be.empty');
-
-        cy.get("#requests")
-          .should('be.empty');
-
-        cy.get("#signature")
-          .should('be.empty');
-
-        cy.get("#agree")
-          .should('be.empty');
-    });
-
-    it("Validando Comandos", () => {
-
-        cy.testeFields(usuario);
-
-        cy.get("button[type='submit']")
-          .as("botaoEnviar")
-          .should("not.be.disabled");
-
-        cy.get("button[type='reset']").click();
-
-        cy.get("@botaoEnviar").should("be.disabled");
+    it("Validando campos obrigatórios e botão reset", () => {
+        cy.botaoEnviarDisabled();
+        cy.validaCamposObrigatorios(usuario);
+        cy.botaoReset();
+        cy.camposVazios();
     });
 
     //beforeEach(() => cy.visit("https://www.drogasil.com.br/"));
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,9 +26,124 @@
 
 //const cypress = require("cypress")
 
-Cypress.Commands.add("testeFields", dados => {
+Cypress.Commands.add("preencheCamposTexto", dados => {
     cy.get("#first-name").type(dados.primeiroNome);
     cy.get("#last-name").type(dados.ultimoNome);
     cy.get("#email").type(dados.email);
-    cy.get("#agree").check(); 
+    cy.get("#requests").type(dados.frase);
+    cy.get("#signature").type(dados.assinatura);
+    //cy.get("#agree").check();
+});
+
+Cypress.Commands.add("preencheCheckbox", () => {
+    cy.get("#friend").check();
+    cy.get("#publication").check();
+    cy.get("#social-media").check();
+});
+
+Cypress.Commands.add("unCheckbox", () => {
+    cy.get("#friend").uncheck();
+    cy.get("#publication").uncheck();
+    cy.get("#social-media").uncheck();
+});
+
+Cypress.Commands.add("validaInformacoesDeTexto", dados => {
+    cy.get("#first-name").type(dados.primeiroNome);
+    cy.get("#last-name").type(dados.ultimoNome);
+
+    cy.get(dados.radio).check();
+    cy.get("#ticket-quantity").select(dados.qtn);
+    
+    const nomeInteiro = `${dados.primeiroNome} ${dados.ultimoNome}`;
+    if (dados.radio == "#general") {
+        if (dados.qtn != "1") {
+            cy.get(".agreement p")
+              .should("contain", `I, ${nomeInteiro}, wish to buy ${dados.qtn} General Admission tickets. I understand that all ticket sales are final.`);
+        } else {
+            cy.get(".agreement p")
+              .should("contain", `I, ${nomeInteiro}, wish to buy 1 General Admission ticket. I understand that all ticket sales are final.`);
+        }
+    } else {
+        if (dados.qtn != "1") {
+            cy.get(".agreement p")
+              .should("contain", `I, ${nomeInteiro}, wish to buy 2 VIP tickets. I understand that all ticket sales are final.`);
+        } else {
+            cy.get(".agreement p")
+              .should("contain", `I, ${nomeInteiro}, wish to buy 1 VIP ticket. I understand that all ticket sales are final.`);
+        } 
+    }
+});
+
+Cypress.Commands.add("botaoEnviarDisabled", () => {
+    cy.get("button[type='submit']").should("be.disabled");
+});
+
+Cypress.Commands.add("botaoEnviarEnabled", () => {
+    cy.get("button[type='submit']").should("not.be.disabled");
+});
+
+Cypress.Commands.add("botaoReset", () =>{
+    cy.get("button[type='reset']").click();
+});
+
+Cypress.Commands.add("validaCamposObrigatorios", dados => {
+    cy.get("#first-name").type(dados.primeiroNome);
+    cy.botaoEnviarDisabled();
+
+    cy.get("#last-name").type(dados.ultimoNome);
+    cy.botaoEnviarDisabled();
+
+    cy.get("#email").type(dados.email);
+    cy.botaoEnviarDisabled();
+
+    cy.get("#requests").type(dados.frase);
+    cy.botaoEnviarDisabled();
+
+    cy.get("#signature").type(dados.assinatura);
+    cy.botaoEnviarDisabled();
+
+    cy.get("#friend").check();
+    cy.botaoEnviarDisabled();
+
+    cy.get("#publication").check();
+    cy.botaoEnviarDisabled();
+
+    cy.get("#social-media").check();
+    cy.botaoEnviarDisabled();
+
+    cy.get("#agree").check();
+    cy.botaoEnviarEnabled();
+});
+
+Cypress.Commands.add("camposVazios", () =>{
+    cy.get("#first-name").should('be.empty');
+    cy.get("#last-name").should('be.empty');
+    cy.get("#email").should('be.empty');
+    cy.get("#friend").should('be.empty');
+    cy.get("#publication").should('be.empty');
+    cy.get("#social-media").should('be.empty');
+    cy.get("#requests").should('be.empty');
+    cy.get("#signature").should('be.empty');
+    cy.get("#agree").should('be.empty');
+});
+
+Cypress.Commands.add("aceitaCheck", () =>{
+    cy.get("#agree").check();
+});
+
+Cypress.Commands.add("validarTitulo", dados =>{
+    cy.get("header h1").should("contain", dados.titulo);
+});
+
+Cypress.Commands.add("validarEmail", dados =>{
+    cy.get("#email")
+      .as("email")
+      .type(dados.emailInvalido);
+
+    cy.get("#email.invalid").should("exist");
+    cy.get("@email")
+      .clear()
+      .type(dados.email);
+
+    cy.get("#email.invalid").should("not.exist");
 });
